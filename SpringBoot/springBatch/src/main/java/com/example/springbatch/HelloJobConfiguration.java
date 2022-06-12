15d22b62bf0c1771bc5feba38fcd9b72d1a38e65
D0loop <riviera-towels0j@icloud.com>
2022-06-13 00:56:51 +0900

    hello spring batch

---
+++ SpringBoot/springBatch/src/main/java/com/example/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,56 @@
+package com.example.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author D0Loop
+ * @since 2022-06-01 [2022.6월.01]
+ */
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return stepBuilderFactory.get("helloStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("====================");
+                    System.out.println("HELLO SPRING BATCH 1");
+                    System.out.println("====================");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("====================");
+                    System.out.println("HELLO SPRING BATCH 2");
+                    System.out.println("====================");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
